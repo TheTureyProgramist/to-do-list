0bf71b7043498f745b7972b65b7e35ee70591bfb
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2025-11-02 10:06:41 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import './App.css';
+import styled from 'styled-components';
+// Створити компоненти 
+// TodoList - для відображення списку завдань, 
+// TodoEditor - форма для додавання нового завдання, 
+// Filter - інпут для фільтрування завдань 
+// Info -  елементи для відображення Загальної кількості завдань та Кількості виконаних
+// Стейт в App повинен виглядати так:
+// state={
+//     todos: initialTodos,
+//     filter: '',
+//   }
 
+
+// Стейт в TodoEditor:
+// state={
+//         textValue: '',
+//     }
+
+
+
+// Стилі додати використовуючи бібліотеку styled-component. Обмежень в стилізаціїї немає.
 function App() {
   return (
     <div className="App">
