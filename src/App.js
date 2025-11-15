ae341b27e20d6f3fe3bed5c07c13643c2bd2ecd7
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2025-11-15 18:57:24 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,65 +1,96 @@
 import './App.css';
-import { initialTodos } from './data/todo.json'
 import { Component } from 'react';
 import styled from 'styled-components';
 import { List } from './components/List/List';
-// Створити новий проєкт на основі реакт або використати готову збірку react-homework-template
-// Створити файл todo.json з наступними даними
-// [
-//   { "id": "id-1", "text": "Вивчити основи React", "completed": true },
-//   { "id": "id-2", "text": "Розібратися з React Router", "completed": false },
-//   { "id": "id-3", "text": "Пережити Redux", "completed": false }
-// ]
-// // Створити компоненти 
-// TodoList - для відображення списку завдань, 
-// TodoEditor - форма для додавання нового завдання, 
-// Filter - інпут для фільтрування завдань 
-// Info -  елементи для відображення Загальної кількості завдань та Кількості виконаних
-// Стейт в App повинен виглядати так:
-// state={
-//     todos: initialTodos,
-//     filter: '',
-//   }
-
-
-// Стейт в TodoEditor:
-// state={
-//         textValue: '',
-//     }
-
-
-
-// Стилі додати використовуючи бібліотеку styled-component. Обмежень в стилізаціїї немає.
-
-const Form = styled.form`
-
-`;
 const Input = styled.input`
-
+  width: 95%;
+  height: 100px;
+  padding-left: 50px;
+  border-radius: 8px;
+  display: flex;
+  margin-top:20px;
+  justify-content: center;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const TextBlock = styled.div`
+  justify-content: space-evenly;
+  display: flex;
+`;
+const AllTasks = styled.h2`
+  display: block;
+`;
+const Completed = styled.h2`
+  display: block;
+`;
+const Block = styled.div`
+  margin-top: auto;
+  margin-bottom: auto;
 `;
 class App extends Component {
-  
   state = {
     todos: [
-  { "id": "id-1", "text": "Вивчити основи React", "completed": true },
-  { "id": "id-2", "text": "Розібратися з React Router", "completed": false },
-  { "id": "id-3", "text": "Пережити Redux", "completed": false }
-],
+      { id: 'id-1', text: 'Вивчити основи React', completed: true },
+      { id: 'id-2', text: 'Розібратися з React Router', completed: false },
+      { id: 'id-3', text: 'Пережити Redux', completed: false }
+    ],
     filter: '',
-  }
-  formChange = evt => {
-     this.setState({filter: evt.target.value}); 
-   
-};
-    render() {
+    inputValue: ''
+  };
+  handleInputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+  addTodo = () => {
+    if (this.state.inputValue.trim() === '') return;
+    const newTodo = {
+      id: Date.now().toString(),
+      text: this.state.inputValue,
+      completed: false
+    };
+    this.setState(prev => ({
+      todos: [...prev.todos, newTodo],
+      inputValue: ''
+    }));
+  };
+  deleteTodo = id => {
+    this.setState(prev => ({
+      todos: prev.todos.filter(todo => todo.id !== id)
+    }));
+  };
+  toggleTodo = id => {
+    this.setState(prev => ({
+      todos: prev.todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    }));
+  };
+  render() {
     return (
-    <div className="App">
-      <></>
-      <Form>
-      <Input></Input>
-      </Form>
-        <List todos={this.state.todos} />
-    </div>
-  );
+      <div className="App">
+        <Block>
+          <TextBlock>
+            <AllTasks>Всього завданнь:</AllTasks>
+            <Completed>Виконані завдання:</Completed>
+          </TextBlock>
+          <Form>
+            <Input
+              placeholder="Уведіть завдання яке вам треба"
+              value={this.state.inputValue}
+              onChange={this.handleInputChange}
+            />
+          </Form>
+          <List
+            onDelete={this.deleteTodo}
+            onToggle={this.toggleTodo}
+            onAdd={this.addTodo}
+            todos={this.state.todos}
+          />
+        </Block>
+      </div>
+    );
   }
 }
+export default App;
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,28 +1,58 @@
-import styled from "styled-components"
-import { todos } from '../../App';
+import styled from "styled-components";
 const Item = styled.li`
-border: black;
 display: flex;
-gap: 200px;
+align-items: center;
+box-shadow: rgba(0, 0, 0, 0.75) 5px 5px, rgba(0, 0, 0, 0.6) 10px 10px, rgba(0, 0, 0, 0.45) 15px 15px;
+border: 1px solid black;
+gap: 15px;
+width: 100%;
+border-radius: 5px;
+border: solid black 5px;
 `;
 const Button = styled.button`
-padding: 15px 30px;
-background-color: green;
+padding: 15px 50px;
+background: green;
+&:hover { background: darkviolet; }
 color: white;
+margin-left: auto;
+cursor: pointer;
 `;
 const ListType = styled.ul`
-padding: 20px;
+display: flex;
+flex-direction: column;
+gap: 25px;
 `;
-export const List = ({}) => {
-    return <ListType>
-        <Item>
-            <input type="checkbox"></input>
-                  {todos.map(todo => (
-            <div key={todo.id}>
-                <span>{todo.text}</span>
-            </div>
-        ))}
-            <Button>Delete</Button>
-        </Item>
-    </ListType>
-}
+const ButtonPlus = styled.button`
+padding-top: 5px;
+padding-bottom: 5px;
+background: yellow;
+box-shadow: rgba(0, 0, 0, 0.75) 5px 5px, rgba(0, 0, 0, 0.6) 10px 10px, rgba(0, 0, 0, 0.45) 15px 15px;
+border: 5px solid black;
+width: 100%;
+border-radius: 5px;
+display: block;
+&:hover { background: skyblue; color: white; }
+color: red;
+cursor: pointer;
+font-size: 35px;
+`;
+export const List = ({ todos, onDelete, onToggle, onAdd }) => {
+return (
+<ListType>
+<ButtonPlus onClick={onAdd}>+</ButtonPlus>
+{todos.map(todo => (
+<Item key={todo.id}>
+<input
+type="checkbox"
+checked={todo.completed}
+onChange={() => onToggle(todo.id)}
+/>
+<span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+{todo.text}
+</span>
+<Button onClick={() => onDelete(todo.id)}>Delete</Button>
+</Item>
+))}
+</ListType>
+);
+};
